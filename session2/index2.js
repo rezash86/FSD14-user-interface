c40b5efa8ae4992ecc9cf9a1d3491a205079550e
rezash86 <reza.sh@gmail.com>
2025-06-17 20:15:14 -0400

    session 2 - reduce

--- session2/index2.js
+++ session2/index2.js
@@ -27,3 +27,30 @@ let modifiedCustomers2 = customers.map((customer) => ({
   custId: "*".repeat(customer.custId.length),
 }));
 console.log(modifiedCustomers2);
+
+const array1 = [1, 2, 3, 4];
+
+// 0 + 1 + 2 + 3 + 4
+const initialValue = 0;
+const sumWithInitial = array1.reduce(
+  (accumulator, currentValue) => accumulator + currentValue
+);
+
+console.log(sumWithInitial);
+
+let myNumbers = [4, 5, 6];
+// const findMin = (a, b) => Math.min(a, b);
+
+console.log(myNumbers.reduce((a, b) => Math.min(a, b), 10));
+
+const array = [15, 16, 17, 18, 19];
+
+function reducer(accumulator, currentValue, index) {
+  const returns = accumulator + currentValue;
+  console.log(
+    `accumulator: ${accumulator}, currentValue: ${currentValue}, index: ${index}, returns: ${returns}`
+  );
+  return returns;
+}
+
+array.reduce(reducer);
