3bd3b01cbc7e1422a6de95eafbe692ca81b5994a
rezash86 <reza.sh@gmail.com>
2025-06-20 19:49:41 -0400

    bootstrap

--- session5/script.js
+++ session5/script.js
@@ -23,3 +23,23 @@ const tooltipTriggerList = document.querySelectorAll(
 const tooltipList = [...tooltipTriggerList].map(
   (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
 );
+
+function showAlert() {
+  document.getElementById("alertPlaceHolder").innerHTML =
+    '<div class="alert alert-success" role="alert"> This is a bootstrap message! </div>';
+}
+
+function toggleHighLight() {
+  //toggle helps me to add a class into the classlist if it does not exist
+  //and if it exists it will remove it
+  console.log(document.getElementById("myText").classList);
+  document.getElementById("myText").classList.toggle("text-danger");
+  console.log(document.getElementById("myText").classList);
+}
+
+function changeColor() {
+  const selected = document.getElementById("colorSelect").value;
+  console.log(selected);
+  const text = document.getElementById("coloredText");
+  text.className = "mt-3 text-" + selected;
+}
